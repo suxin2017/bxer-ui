e79b3782bb4284c2061560221eca64cd32d1168a
bxer <yishidixiashang@gmail.com>
2020-03-19 14:56:42 +0800

    feature: 日期选择

--- src/components/datepicker/calendar.jsx
+++ src/components/datepicker/calendar.jsx
@@ -1,23 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './calendar.sass'
 import Icon from "../icon";
+import {date} from "@storybook/addon-knobs";
+import {getDates, isEquel} from "./util";
 
-Calendar.propTypes = {
-
-};
+Calendar.propTypes = {};
 
 function Calendar(props) {
-    const {selected,key,value,children} = props;
-    let optionClassName = classNames('bxer-calendar',
-        {
-            "bxer-option__selected":selected,
-        });
+    const {currentYear, currentMonth, value,onChange=()=>{}} = props;
 
-    return (
-       123
-    );
+    const [currentDay, setCurrentDay] = useState({});
+    let weekTh = ['一', '二', '三', '四', '五', '六', '日']
+    let dates = getDates('2020', '5');
+
+
+    return <table className={'bxer-calendar'}>
+        <thead className={'bxer-calendar__head'}>
+        <tr>
+            {weekTh.map(item => <th key={item}>{item}</th>)}
+        </tr>
+        </thead>
+        <tbody className={'bxer-calendar__body'}>
+        {
+            [0, 1, 2, 3, 4, 5].map((index) => {
+                return <tr key={index} className={'bxer-calendar__body-row'}>
+                    {dates.slice(index * 7, index * 7 + 7).map((item) => {
+                        return <td
+                            onClick={() => {
+                                if(item.type === 'current'){
+                                    setCurrentDay(item)
+                                    onChange(item)
+                                }
+                            }}
+                            className={classNames('bxer-calendar__body-col',
+                                `bxer-calendar__body-col__${item.type}`,
+                                {
+                                    'bxer-calendar__body-col__selected':
+                                        isEquel(currentDay,item)
+                                }
+                                )}
+                            key={index}>{item.day}</td>
+                    })}
+                </tr>
+            })
+        }
+        </tbody>
+    </table>;
 }
 
 
