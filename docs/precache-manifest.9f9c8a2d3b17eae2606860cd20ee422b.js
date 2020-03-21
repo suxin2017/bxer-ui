ca484db0bea87b312654a15472dc648c77358f32
bxer <xin.wen@baifendian.com>
2020-03-21 10:35:50 +0800

    chroe: 更新website 兼容github

---
+++ docs/precache-manifest.9f9c8a2d3b17eae2606860cd20ee422b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac16cb16e6a3447bd5df5793211e45d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bb8a987e811568207a97.bundle.js"
+  },
+  {
+    "url": "runtime~main.bb8a987e811568207a97.bundle.js"
+  },
+  {
+    "revision": "6162213ead26ad10765b",
+    "url": "static/css/main.dce583c3.chunk.css"
+  },
+  {
+    "revision": "fac69ea27b3b43065bb5",
+    "url": "static/css/vendors~main.ad6f07ce.chunk.css"
+  },
+  {
+    "revision": "3b5b92922e0d20f0b6a05ca30137488f",
+    "url": "static/media/remixicon.3b5b9292.eot"
+  },
+  {
+    "revision": "76414508f73cb3fb57073b235c53fa6c",
+    "url": "static/media/remixicon.76414508.woff2"
+  },
+  {
+    "revision": "c0d34ad379cbf1557481a30be08263ff",
+    "url": "static/media/remixicon.c0d34ad3.ttf"
+  },
+  {
+    "revision": "d0ac4bc43acfbe23145c4e5730f48c7f",
+    "url": "static/media/remixicon.d0ac4bc4.svg"
+  },
+  {
+    "revision": "e5b755b03df25eabc565694ec9aad29f",
+    "url": "static/media/remixicon.e5b755b0.woff"
+  },
+  {
+    "url": "vendors~main.bb8a987e811568207a97.bundle.js"
+  },
+  {
+    "url": "vendors~main.bb8a987e811568207a97.bundle.js.LICENSE.txt"
+  }
+]);
