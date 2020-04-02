7e0ad49550666186e1e5c0da929ad0931fdef194
bxer <xin.wen@baifendian.com>
2020-04-02 17:17:41 +0800

    build: gulp 配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-const { series, src, dest,parallel } = require("gulp");
-const path = require("path");
+const {series=()=>{}, src=()=>{}, dest=()=>{}} = require("gulp");
 const clean = require("gulp-clean");
 
 function cleanCache() {
-    return src("lib", { read: false,allowEmpty:true }).pipe(clean());
+    return src("lib", {read: false, allowEmpty: true}).pipe(clean());
 }
 
 function compileSass() {
@@ -11,11 +10,11 @@ function compileSass() {
     const syntax = require("postcss-less");
     const sass = require('gulp-sass');
 
-    return src("src/**/*.sass")
+    return src("src/components/**/*.sass")
         .pipe(
             sass()
         )
-        .pipe(postcss([require("autoprefixer")], { syntax: syntax }))
+        .pipe(postcss([require("autoprefixer")], {syntax: syntax}))
         .pipe(dest("lib/"));
 }
 
@@ -24,16 +23,15 @@ function complie() {
     const babel = require('gulp-babel');
     const replace = require('gulp-replace');
 
-    return src(['src/**/*.js','src/**/*.jsx','!src/**/*.test.js'])
+    return src(['src/components/**/*.js', 'src/components/**/*.jsx', '!src/components/**/*.test.js'])
         .pipe(babel({
-            presets: ['@babel/env','@babel/react']
+            presets: ['@babel/env', '@babel/react']
 
         }))
-        .pipe(replace('.sass','.css'))
-
+        .pipe(replace('.sass', '.css'))
         .pipe(dest('lib/'))
 }
 
 
 exports.default = series(cleanCache,
-    series(compileSass,complie));
+    series(compileSass, complie));
