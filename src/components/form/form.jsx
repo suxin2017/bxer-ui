2170182f5902bf671b57cf35bc7359bb94fa9099
bxer <yishidixiashang@gmail.com>
2020-04-08 16:23:05 +0800

    fix: form 初始化校验报错

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,97 +1,105 @@
 import React, {
-    useContext,
-    useEffect,
-    useReducer,
-    useRef,
-    useState
-} from 'react';
-import {get, set, verifyRules, versifyData} from "./util";
+  useContext,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
+import { get, set, verifyRules, versifyData } from "./util";
 import PropTypes from "prop-types";
 
 Form.propTypes = {
-    /**
-     * 每一行为一个 [Row](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
-     * 氛围wrapper 和 label 两部分
-     * 布局参数请参考  [Layout](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
-     * Col部分
-     */
-    wrapperSpan: PropTypes.object,
-    /**
-     * 每一行为一个 [Row](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
-     * 氛围wrapper 和 label 两部分
-     * 布局参数请参考  [Layout](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
-     * Col部分
-     */
-    labelSpan: PropTypes.object,
-    /**
-     * 表单回显数据
-     */
-    data: PropTypes.object,
-    /**
-     * 提交回调
-     */
-    onSubmit: PropTypes.func,
+  /**
+   * 每一行为一个 [Row](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
+   * 氛围wrapper 和 label 两部分
+   * 布局参数请参考  [Layout](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
+   * Col部分
+   */
+  wrapperSpan: PropTypes.object,
+  /**
+   * 每一行为一个 [Row](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
+   * 氛围wrapper 和 label 两部分
+   * 布局参数请参考  [Layout](/?path=/docs/layout--%E5%9F%BA%E6%9C%AC%E7%94%A8%E6%B3%95)
+   * Col部分
+   */
+  labelSpan: PropTypes.object,
+  /**
+   * 表单回显数据
+   */
+  data: PropTypes.object,
+  /**
+   * 提交回调
+   */
+  onSubmit: PropTypes.func,
 };
 
-
 function Form(props) {
-    const {children,wrapperSpan,labelSpan, data: propData} = props;
-    const [data, setData] = useState({});
-    const rules = useRef({});
-    const [validate, setValidate] = useState({});
+  const { children, wrapperSpan, labelSpan, data: propData } = props;
+  const [data, setData] = useState({});
+  const rules = useRef({});
+  const [validate, setValidate] = useState({});
 
-    const injectFormItem = React.Children.map(children, formItem => {
-        const {dataKey = '', label, rules: formItemRules = [],
-            isRequest,
-            wrapperSpan:formItemWrapperSpan,
-            labelSpan:formItemLabelSpan} = formItem.props;
+  const injectFormItem = React.Children.map(children, (formItem) => {
+    const {
+      dataKey = "",
+      label,
+      rules: formItemRules = [],
+      isRequest,
+      wrapperSpan: formItemWrapperSpan,
+      labelSpan: formItemLabelSpan,
+    } = formItem.props;
 
-        if (rules && formItemRules) {
-            set(rules.current, dataKey, formItemRules)
-        }
-        if (isRequest) {
-            formItemRules.push({
-                rule: function (value) {
-                    return value
-                },
-                message: `${label}不能为空`
-            });
-        }
-
-        const formItemValue = get(data, dataKey, undefined);
-        const formItemProps = {
-            value: formItemValue,
-            rules: formItemRules,
-            wrapperSpan:formItemWrapperSpan || wrapperSpan,
-            labelSpan:formItemLabelSpan || labelSpan,
-            verify: get(validate, dataKey),
-            onChange: (value) => {
-                const newData = set(data, dataKey, value);
-                setData(newData)
-            }
-        };
-
-        return React.cloneElement(formItem, formItemProps)
-    });
-
-    function handleVerify() {
-        setValidate(versifyData(data, rules.current))
+    if (rules && formItemRules) {
+      set(rules.current, dataKey, formItemRules);
+    }
+    if (isRequest) {
+      formItemRules.push({
+        rule: function(value) {
+          return value;
+        },
+        message: `${label}不能为空`,
+      });
     }
 
-    useEffect(() => {
-        setData(propData)
-    }, [propData])
+    const formItemValue = get(data, dataKey, undefined);
+    const formItemProps = {
+      value: formItemValue,
+      rules: formItemRules,
+      wrapperSpan: formItemWrapperSpan || wrapperSpan,
+      labelSpan: formItemLabelSpan || labelSpan,
+      verify: get(validate, dataKey),
+      onChange: (value) => {
+        const newData = set(data, dataKey, value);
+        setData(newData);
+      },
+    };
 
+    return React.cloneElement(formItem, formItemProps);
+  });
 
-    return (
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            props.onSubmit && props.onSubmit(data);
-            handleVerify();
-        }}>
-            {injectFormItem}
-        </form>
-    );
+  function handleVerify() {
+    setValidate(versifyData(data, rules.current));
+  }
+  useEffect(() => {
+    console.log(data, "data");
+  }, [data]);
+  useEffect(() => {
+    if (propData != null) {
+      setData(propData);
+    }
+  }, [propData]);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.onSubmit && props.onSubmit(data);
+        handleVerify();
+      }}
+    >
+      {injectFormItem}
+    </form>
+  );
 }
 
 export default Form;
--- src/components/form/util.js
+++ src/components/form/util.js
@@ -1,8 +1,7 @@
-import {isArray, isRegExp} from "../util/type";
+import { isArray, isRegExp } from "../util/type";
 
-const pathRegex = /\[(\d*)\]/ig;
-const getPath = (keyStr) => keyStr.replace(pathRegex, '.$1').split(".");
-
+const pathRegex = /\[(\d*)\]/gi;
+const getPath = (keyStr) => keyStr.replace(pathRegex, ".$1").split(".");
 
 /**
  *
@@ -15,46 +14,43 @@ const getPath = (keyStr) => keyStr.replace(pathRegex, '.$1').split(".");
  *  这种情况
  */
 export function get(obj, keyStr, defaultValue) {
-    let result = undefined;
-    if (obj == null) {
-        result = undefined;
-    } else {
-        let path = getPath(keyStr);
-        let _obj = obj;
-        let index = 0;
-        while (_obj != null && index < path.length) {
-            _obj = _obj[path[index++]];
-        }
-        result = _obj;
+  let result = undefined;
+  if (obj == null) {
+    result = undefined;
+  } else {
+    let path = getPath(keyStr);
+    let _obj = obj;
+    let index = 0;
+    while (_obj != null && index < path.length) {
+      _obj = _obj[path[index++]];
     }
-    return result === undefined ? defaultValue : result;
-
+    result = _obj;
+  }
+  return result === undefined ? defaultValue : result;
 }
-
 
 /**
  *
  */
 export function set(obj, keyStr, value) {
-    let path = getPath(keyStr);
-    let _obj = obj || {};
-    let header = _obj;
-    let index = 0;
-    console.log(path)
-    while (index < path.length - 1) {
-        if (_obj[path[index]] == null) {
-            const nextIsNumber = !isNaN(Number(path[index + 1]));
-            if (nextIsNumber) {
-                _obj[path[index]] = [];
-            } else {
-                _obj[path[index]] = {};
-            }
-
-        }
-        _obj = _obj[path[index++]]
+  let path = getPath(keyStr);
+  let _obj = obj || {};
+  let header = _obj;
+  let index = 0;
+  console.log(path);
+  while (index < path.length - 1) {
+    if (_obj[path[index]] == null) {
+      const nextIsNumber = !isNaN(Number(path[index + 1]));
+      if (nextIsNumber) {
+        _obj[path[index]] = [];
+      } else {
+        _obj[path[index]] = {};
+      }
     }
-    _obj[path[index]] = value;
-    return Object.assign({}, header)
+    _obj = _obj[path[index++]];
+  }
+  _obj[path[index]] = value;
+  return Object.assign({}, header);
 }
 
 /**
@@ -64,23 +60,22 @@ export function set(obj, keyStr, value) {
  * @returns {*}
  */
 export function verifyRules(rules, value) {
-    if (isArray(rules)) {
-        for (let i = 0; i < rules.length; i++) {
-            const {rule} = rules[i];
-            // 正则时候
-            if (isRegExp(rule)) {
-                if (!rule.test(value)) {
-                    return rules[i]
-                }
-            } else {
-                // 函数的时候
-                if (!rule(value)) {
-                    return rules[i]
-                }
-            }
-
+  if (isArray(rules)) {
+    for (let i = 0; i < rules.length; i++) {
+      const { rule } = rules[i];
+      // 正则时候
+      if (isRegExp(rule)) {
+        if (!rule.test(value)) {
+          return rules[i];
         }
+      } else {
+        // 函数的时候
+        if (!rule(value)) {
+          return rules[i];
+        }
+      }
     }
+  }
 }
 
 /**
@@ -90,15 +85,15 @@ export function verifyRules(rules, value) {
  * @returns {{}}
  */
 export function versifyData(data, rules) {
-    const verifies = {}
+  const verifies = {};
 
-    for (let key of Object.keys(rules)) {
-        if (Object.prototype.hasOwnProperty.call(rules, key)) {
-            const rule = rules[key];
-            const ruleData = data[key];
-            const verify = verifyRules(rule, ruleData)
-            verifies[key] = verify;
-        }
+  for (let key of Object.keys(rules)) {
+    if (Object.prototype.hasOwnProperty.call(rules, key)) {
+      const rule = rules[key];
+      const ruleData = data[key];
+      const verify = verifyRules(rule, ruleData);
+      verifies[key] = verify;
     }
-    return verifies;
+  }
+  return verifies;
 }
